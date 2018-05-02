eea6d280b680883673615439d27b11f28726daae
NoSkillGuy <rsivapraveen001@gmail.com>
2018-05-02 12:31:53 +0530

    Basic ubuntu setup script

---
+++ ubuntu_setup.sh
@@ -0,0 +1,19 @@
+# updating apt-get 
+sudo apt-get update
+
+# Installing curl 
+sudo apt -y install curl
+
+# Installing terminator.
+sudo apt -y install terminator
+
+# Installing python3, pip3 and many other dependencies.
+sudo apt -y instll python3-pip
+
+# Intalling mysql 
+sudo apt -y install mysql-server
+
+# Installing locate
+# Usage: locate filename or keyboard of the filename
+sudo apt install locate
+
